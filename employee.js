991b5fe5f501e44dd14c80a73ca733c16e909ef8
Brian Washington <bw49078@gmail.com>
2022-10-20 19:31:53 -0500

    added employee class

---
+++ employee.js
@@ -0,0 +1,12 @@
+class Employee {
+    constructor(name, salary, title, manager = null) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager;
+    }
+
+
+}
+
+module.exports = Employee;
